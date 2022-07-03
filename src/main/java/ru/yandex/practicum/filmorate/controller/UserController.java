10eab71dccdbd0fe18fa9ca63e4d878513356b34
Linar <latypov.ld@yandex.ru>
2022-07-03 18:31:35 +0500

    создать каркас springboot приложения filmorate по ТЗ спринта 8. 1-ая итерация проверки.

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,17 +1,19 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
+import javax.validation.Valid;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@Slf4j
 @RequestMapping("/users")
 public class UserController {
-    private final Map<String, User> users = new HashMap<>();
+    private final Map<Integer, User> users = new HashMap<>();
 
     @GetMapping
     public Collection<User> findAll() {
@@ -19,25 +21,37 @@ public class UserController {
     }
 
     @PostMapping
-    public User create(@RequestBody User user) {
-        if(user.getEmail() == null || user.getEmail().isBlank()) {
-            throw new ValidationException("Адрес электронной почты не может быть пустым.");
+    public User createUser(@RequestBody @Valid User user) {
+        user.setId(users.size() + 1);
+        validate(user);
+        if(user.getName().isBlank()) {
+            user.setName(user.getLogin());
         }
-        if(users.containsKey(user.getEmail())) {
-            throw new ValidationException("Пользователь с электронной почтой " +
-                    user.getEmail() + " уже зарегистрирован.");
-        }
-        users.put(user.getEmail(), user);
+        users.put(user.getId(), user);
+        log.info("Добавлен новый пользователь: '{}', ID '{}', '{}'", user.getName(), user.getId(), user.getEmail());
         return user;
     }
 
     @PutMapping
-    public User put(@RequestBody User user) {
-        if(user.getEmail() == null || user.getEmail().isBlank()) {
-            throw new ValidationException("Адрес электронной почты не может быть пустым.");
+    public User updateUser(@RequestBody @Valid User user) {
+        if(users.containsKey(user.getId())) {
+            validate(user);
+            if (user.getName().isBlank()) {
+                user.setName(user.getLogin());
+            }
+            users.put(user.getId(), user);
+            log.info("Внесены изменения в данные пользователя: '{}', ID '{}', '{}'",
+                    user.getName(), user.getId(), user.getEmail());
+            return user;
+        } else {
+            throw new RuntimeException("Пользователь с данным Id  не найден");
         }
-        users.put(user.getEmail(), user);
+    }
 
-        return user;
+    void validate(User user) {
+        if(user.getLogin().contains(" ")) {
+            throw new RuntimeException("Логин содержит пробелы.");
+        }
+        log.info("Проведена валидация данных пользователя: '{}'", user);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,13 +2,21 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
 public class User {
-    private int id;
+    private Integer id;
+    @NotNull(message = "Адрес электронной почты пользователя не может быть пустым.")
+    @NotBlank(message = "Адрес электронной почты пользователя не может быть пустым.")
+    @Email(message = "Адрес электронной почты пользователя некорректен.")
     private String email;
+    @NotNull(message = "Логин пользователя не может быть пустым.")
+    @NotBlank(message = "Логин пользователя не может быть пустым.")
     private String login;
+
     private String name;
-    private LocalDate birhtday;
+    @Past(message = "Дата рождения не может быть в будущем.")
+    private LocalDate birthday;
 }
